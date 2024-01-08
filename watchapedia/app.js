42f006836bbe5d69c28b4fe8351a9e3221641a47
kimsohyeon <aotoyae@gmail.com>
2024-01-08 12:07:32 +0900

    feat: 키 가져오는거 수정

--- watchapedia/app.js
+++ watchapedia/app.js
@@ -1,17 +1,19 @@
-const API_KEY = options.Authorization;
+const movieContent = document.getElementById("movie-content");
+const API_KEY = apiKey.headers;
 
 const options = {
   method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: API_KEY,
-  },
+  API_KEY,
 };
 
-fetch(
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+function dispaly() {
+  fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+}
+
+dispaly();
