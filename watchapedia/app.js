2fb06819282a4b90cc70c4ac1b0280be2d00a1a1
kimsohyeon <aotoyae@gmail.com>
2024-01-09 18:42:16 +0900

    add: css 수정, 검색 기능 진행 중

--- watchapedia/app.js
+++ watchapedia/app.js
@@ -1,9 +1,10 @@
 const movieContainer = document.getElementById("movie-container");
 const movieContainerUl = document.getElementById("movie-list");
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
 
 const page = 1;
 const API_KEY = apiKey.headers;
-
 const BASE_URL = "https://api.themoviedb.org/3/movie/top_rated?language=en-US";
 const FULL_URL = `${BASE_URL}&page=${page}`;
 
@@ -12,38 +13,83 @@ const options = {
   API_KEY,
 };
 
-function getApi() {
+const getMovies = () => {
   fetch(FULL_URL, options)
     .then((response) => response.json())
-    .then((json) => dispaly(json))
-    .catch((err) => console.error(err));
-}
+    .then((movieList) => displayMovies(movieList))
+    .catch((err) => {
+      console.error(err);
+      window.alert(`ERROR!`);
+    });
+};
 
-getApi();
+getMovies();
 
-function dispaly(json) {
-  let movies = json.results;
-  console.log(movies);
+const displayMovies = (movieList) => {
+  let movies = movieList.results;
   movies.forEach((movie) => {
     let overview = movie.overview;
     let imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    let classMovieInpo = "movie-info";
-    let classMovieDate = "movie-date";
-    let classMovieTitle = "movie-title";
-    let classMovieOverview = "movie-overview";
 
     movieContainerUl.innerHTML += `
-    <li>
-      <img src ="${imageUrl}">
-      <div class=${classMovieInpo}>
-        <h3 class=${classMovieTitle}>
-        ${movie.title}
-        </h3>
-        <p class=${classMovieOverview}>${
-      overview.length > 100 ? overview.slice(0, 100) + `...` : overview
-    }</p>
-        <h5 class=${classMovieDate}>${movie.release_date}</h5>
-      </div>
-    </li>`;
+      <li class = "movie-card">
+        <img src ="${imageUrl}">
+        <div class= "movie-info">
+          <h3 class= "movie-title">
+          ${movie.title}
+          </h3>
+          <p class= "movie-average">${
+            Math.ceil(movie.vote_average * 10) / 10
+          }</p>
+          <p class= "movie-overview">${
+            overview.length > 100 ? overview.slice(0, 100) + `...` : overview
+          }</p>
+          <h5 class= "movie-date"}>${movie.release_date}</h5>
+        </div>
+      </li>`;
   });
-}
+};
+
+const searchMovies = () => {
+  fetch(FULL_URL, options)
+    .then((response) => response.json())
+    .then((movieList) => {
+      let movies = movieList.results;
+      const searchTitle = searchInput.value.toLowerCase();
+
+      movies
+        .filter((movie) => movie.title.toLowerCase() === searchTitle)
+        .forEach((movie) => {
+          let overview = movie.overview;
+          let imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+          movieContainerUl.innerHTML += `
+            <li class = "movie-card">
+              <img src ="${imageUrl}">
+              <div class= "movie-info">
+                <h3 class= "movie-title">
+                ${movie.title}
+                </h3>
+                <p class= "movie-average">${
+                  Math.ceil(movie.vote_average * 10) / 10
+                }</p>
+                <p class= "movie-overview">${
+                  overview.length > 100
+                    ? overview.slice(0, 100) + `...`
+                    : overview
+                }</p>
+                <h5 class= "movie-date"}>${movie.release_date}</h5>
+              </div>
+            </li>`;
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      window.alert(`ERROR!`);
+    });
+};
+
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  searchMovies();
+});
