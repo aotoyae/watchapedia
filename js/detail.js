9f0af9c6c5fcd5116fbe667d7dd348499aac71fe
kimsohyeon <aotoyae@gmail.com>
2024-03-06 16:28:04 +0900

    Design: 디테일 페이지 css 수정

--- js/detail.js
+++ js/detail.js
@@ -1,15 +1,15 @@
-import { AUTHORIZE_KEY } from "./key.js";
-import { BASE_URL, IMAGE_URL } from "./url.js";
+import { AUTHORIZE_KEY } from './key.js';
+import { BASE_URL, IMAGE_URL } from './url.js';
 
 const urlParams = new URLSearchParams(location.search);
-const movieId = urlParams.get("id");
+const movieId = urlParams.get('id');
 
 const FULL_URL = `${BASE_URL}movie/${movieId}?language=en-US&`;
 
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    accept: "application/json",
+    accept: 'application/json',
     Authorization: AUTHORIZE_KEY,
   },
 };
@@ -31,33 +31,46 @@ const fetchMovie = async (url) => {
 fetchMovie(FULL_URL);
 
 const displayMovie = (movie) => {
-  const POSTER_ARTICLE = document.getElementById("poster-article");
-  const INFO_ARTICLE = document.getElementById("info-article");
-  const TITLE_BOX = document.createElement("section");
-  TITLE_BOX.classList.add("title-section");
-  const DETAIL_BOX = document.createElement("section");
-  DETAIL_BOX.classList.add("detail-section");
+  const POSTER_ARTICLE = document.getElementById('poster-article');
+  const INFO_ARTICLE = document.getElementById('info-article');
+
+  const TITLE_BOX = document.createElement('section');
+  TITLE_BOX.classList.add('title-section');
+  const BACKDROP_BOX = document.createElement('section');
+  BACKDROP_BOX.classList.add('backdrop-section');
+  const DETAIL_BOX = document.createElement('section');
+  DETAIL_BOX.classList.add('detail-section');
+
+  console.log(movie);
 
   POSTER_ARTICLE.innerHTML = `
-      <img src=${IMAGE_URL + movie.poster_path}/>`;
+      <img src=${IMAGE_URL + movie.poster_path}/>
+  `;
 
   TITLE_BOX.innerHTML = `
-      <h1>${movie.title}
-      </h1>
-      <p class="releas-date">${movie.release_date}</p>
-      <p class="vote-star">
+      <h1 class='movie-title'>${movie.title}</h1>
+      <p class='vote-star'>
         ${getVoteAverage(movie.vote_average)}
-      </p>`;
+      </p>
+  `;
+
+  BACKDROP_BOX.innerHTML = `
+      <img src=${IMAGE_URL + movie.backdrop_path}/>
+      <p class='tag-line'>'${movie.tagline}'</p>
+      `;
 
   DETAIL_BOX.innerHTML = `
-      <p class="tag-line">"${movie.tagline}"</p>
-      <p class="plot">${movie.overview}</p>
-      <a class="homepage-btn" href="${movie.homepage}" target="_blank">
-        click
-      </a>
+      <p class='plot'>${movie.overview}</p>
+      <div class='side-info'>
+        <p class='releas-date'>${movie.release_date}</p>
+        <a class='homepage-btn' href='${movie.homepage}' target='_blank'>
+          click
+        </a>
+      </div>
   `;
 
   INFO_ARTICLE.appendChild(TITLE_BOX);
+  INFO_ARTICLE.appendChild(BACKDROP_BOX);
   INFO_ARTICLE.appendChild(DETAIL_BOX);
 };
 
